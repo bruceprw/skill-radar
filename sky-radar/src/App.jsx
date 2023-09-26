218d04e0e05fcf0e49f268f9e0b76aaabf9dad71
SextonJ <jakesexton1@hotmail.co.uk>
2023-09-26 12:31:18 +0100

    Update Page

--- sky-radar/src/App.jsx
+++ sky-radar/src/App.jsx
@@ -3,17 +3,13 @@ import HomePage from "./Pages/HomePage.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/secondpage" element={<SecondPage />} />
       </Routes>
     </Router>
-
     </>
   )
 }
