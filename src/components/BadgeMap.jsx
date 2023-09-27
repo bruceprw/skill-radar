a6c5b9845c180555107eae22aa46468520bbe702
Harry Gardner <harrygardner123@gmail.com>
2023-09-27 21:40:12 +0100

    add skill icons

--- src/components/BadgeMap.jsx
+++ src/components/BadgeMap.jsx
@@ -1,39 +1,64 @@
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import CodeIcon from '@mui/icons-material/Code';
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import FlagIcon from '@mui/icons-material/Flag';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+import HealingIcon from '@mui/icons-material/Healing';
+import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ExtensionIcon from '@mui/icons-material/Extension';
+import SportsIcon from '@mui/icons-material/Sports';
+import SportsMmaIcon from '@mui/icons-material/SportsMma';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import CakeIcon from '@mui/icons-material/Cake';
+import RestaurantMenuIcon from '@mui/icons-material/RestaurantMenu';
+import DownhillSkiingIcon from '@mui/icons-material/DownhillSkiing';
+import HikingIcon from '@mui/icons-material/Hiking';
+import PrintIcon from '@mui/icons-material/Print';
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import ScubaDivingIcon from '@mui/icons-material/ScubaDiving';
+import BorderAllIcon from '@mui/icons-material/BorderAll';
+import BoltIcon from '@mui/icons-material/Bolt';
+import WysiwygIcon from '@mui/icons-material/Wysiwyg';
+
 const BadgeMap = () => {
 
  const map =[
   {"id": "1", "cat":"1", "title":"Python Programming", "icon":"CodeIcon"},
-  {"id": "2", "cat":"2", "title":"Motor Biking", "icon":"ICON"},
-  {"id": "4", "cat":"1", "title":"C++ Programmingg", "icon":"ICON"},
-  {"id": "5", "cat":"5", "title":"French", "icon":"ICON"},
-  {"id": "6", "cat":"5", "title":"Spanish", "icon":"ICON"},
-  {"id": "7", "cat":"9", "title":"First Aider", "icon":"ICON"},
-  {"id": "8", "cat":"9", "title":"Physiotherapist", "icon":"ICON"},
-  {"id": "9", "cat":"4", "title":"Chef", "icon":"ICON"},
-  {"id": "10", "cat":"8", "title":"Power BI", "icon":"ICON"},
-  {"id": "11", "cat":"7", "title":"Figma", "icon":"ICON"},
-  {"id": "12", "cat":"2", "title":"Referee", "icon":"ICON"},
-  {"id": "13", "cat":"2", "title":"Coach", "icon":"ICON"},
-  {"id": "14", "cat":"2", "title":"Goalkeeper", "icon":"ICON"},
-  {"id": "15", "cat":"3", "title":"Xbox", "icon":"ICON"},
-  {"id": "16", "cat":"3", "title":"Counter-Strike", "icon":"ICON"},
-  {"id": "17", "cat":"3", "title":"Nintendo", "icon":"ICON"},
-  {"id": "18", "cat":"4", "title":"Baking", "icon":"ICON"},
-  {"id": "19", "cat":"4", "title":"Nutrition", "icon":"ICON"},
-  {"id": "20", "cat":"5", "title":"German", "icon":"ICON"},
-  {"id": "21", "cat":"5", "title":"Arabic", "icon":"ICON"},
-  {"id": "22", "cat":"6", "title":"Skiing", "icon":"ICON"},
-  {"id": "23", "cat":"6", "title":"Rock Climbing", "icon":"ICON"},
-  {"id": "24", "cat":"6", "title":"Hill Walking", "icon":"ICON"},
-  {"id": "25", "cat":"6", "title":"Snow boarding", "icon":"ICON"},
-  {"id": "26", "cat":"7", "title":"Fusion 360", "icon":"ICON"},
-  {"id": "27", "cat":"7", "title":"Google Sketchup", "icon":"ICON"},
-  {"id": "28", "cat":"7", "title":"3D Printing", "icon":"ICON"},
-  {"id": "29", "cat":"8", "title":"Data Analysis", "icon":"ICON"},
-  {"id": "30", "cat":"6", "title":"Diving", "icon":"ICON"},
-  {"id": "31", "cat":"9", "title":"Child First-Aid", "icon":"ICON"},
-  {"id": "32", "cat":"8", "title":"Excel", "icon":"ICON"},
-  {"id": "33", "cat":"10", "title":"Software Engineering", "icon":"ICON"},
-  {"id": "34", "cat":"10", "title":"Electrical Engineering", "icon":"ICON"},
+  {"id": "2", "cat":"2", "title":"Motor Biking", "icon":"TwoWheelerIcon"},
+  {"id": "4", "cat":"1", "title":"C++ Programmingg", "icon":"TerminalIcon"},
+  {"id": "5", "cat":"5", "title":"French", "icon":"FlagIcon"},
+  {"id": "6", "cat":"5", "title":"Spanish", "icon":"FlagIcon"},
+  {"id": "7", "cat":"9", "title":"First Aider", "icon":"MedicalServicesIcon"},
+  {"id": "8", "cat":"9", "title":"Physiotherapist", "icon":"HealingIcon"},
+  {"id": "9", "cat":"4", "title":"Chef", "icon":"OutdoorGrillIcon"},
+  {"id": "10", "cat":"8", "title":"Power BI", "icon":"DonutLargeIcon"},
+  {"id": "11", "cat":"7", "title":"Figma", "icon":"ExtensionIcon"},
+  {"id": "12", "cat":"2", "title":"Referee", "icon":"SportsIcon"},
+  {"id": "13", "cat":"2", "title":"Coach", "icon":"SportsIcon"},
+  {"id": "14", "cat":"2", "title":"Goalkeeper", "icon":"SportsMmaIcon"},
+  {"id": "15", "cat":"3", "title":"Xbox", "icon":"SportsEsportsIcon"},
+  {"id": "16", "cat":"3", "title":"Counter-Strike", "icon":"SportsEsportsIcon"},
+  {"id": "17", "cat":"3", "title":"Nintendo", "icon":"SportsEsportsIcon"},
+  {"id": "18", "cat":"4", "title":"Baking", "icon":"CakeIcon"},
+  {"id": "19", "cat":"4", "title":"Nutrition", "icon":"RestaurantMenuIcon"},
+  {"id": "20", "cat":"5", "title":"German", "icon":"FlagIcon"},
+  {"id": "21", "cat":"5", "title":"Arabic", "icon":"FlagIcon"},
+  {"id": "22", "cat":"6", "title":"Skiing", "icon":"DownhillSkiingIcon"},
+  {"id": "23", "cat":"6", "title":"Rock Climbing", "icon":"HikingIcon"},
+  {"id": "24", "cat":"6", "title":"Hill Walking", "icon":"HikingIcon"},
+  {"id": "25", "cat":"6", "title":"Snow boarding", "icon":"AcUnitIcon"},
+  {"id": "26", "cat":"7", "title":"Fusion 360", "icon":"PrintIcon"},
+  {"id": "27", "cat":"7", "title":"Google Sketchup", "icon":"DesignServicesIcon"},
+  {"id": "28", "cat":"7", "title":"3D Printing", "icon":"PrintIcon"},
+  {"id": "29", "cat":"8", "title":"Data Analysis", "icon":"ManageSearchIcon"},
+  {"id": "30", "cat":"6", "title":"Diving", "icon":"ScubaDivingIcon"},
+  {"id": "31", "cat":"9", "title":"Child First-Aid", "icon":"MedicalServicesIcon"},
+  {"id": "32", "cat":"8", "title":"Excel", "icon":"BorderAllIcon"},
+  {"id": "33", "cat":"10", "title":"Software Engineering", "icon":"WysiwygIcon"},
+  {"id": "34", "cat":"10", "title":"Electrical Engineering", "icon":"BoltIcon"},
 ];
 
 return map;
