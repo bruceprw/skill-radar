0b49e85e2a7b2903dadf59dcc332b4331cd2623d
lukedale90 <51388504+lukedale90@users.noreply.github.com>
2023-09-28 11:04:58 +0100

    Skillcard update

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -1,17 +1,12 @@
 import * as React from "react";
 import Avatar from "@mui/joy/Avatar";
-import Chip from "@mui/joy/Chip";
 import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import ButtonGroup from "@mui/joy/ButtonGroup";
 import Card from "@mui/joy/Card";
 import CardContent from "@mui/joy/CardContent";
 import CardOverflow from "@mui/joy/CardOverflow";
-import CardActions from "@mui/joy/CardActions";
-import IconButton from "@mui/joy/IconButton";
+
 import Typography from "@mui/joy/Typography";
-import SvgIcon from "@mui/joy/SvgIcon";
-import UserBadges from "./UserBadges";
+
 import { getBadgeIcon } from "./BadgeMapper";
 
 export default function SkillCard(props) {
@@ -24,9 +19,20 @@ export default function SkillCard(props) {
       }}
     >
       <CardContent sx={{ alignItems: "center", textAlign: "center" }}>
-        <Avatar> {props.description}</Avatar>
-        <Avatar> {icon}</Avatar>
-        <Typography level="title-lg">{props.title}</Typography>
+      <Avatar>{icon}</Avatar>
+
+        <Typography level="title-lg" sx={{my: '0.5em'}}>{props.title}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: 'center',
+            width: "100%",
+          }}
+        >
+          <Avatar sx={{mr: '0.5em'}}>{props.description}</Avatar>
+          <div>Interested</div>
+        </Box>
       </CardContent>
       <CardOverflow sx={{ bgcolor: "background.level1" }}></CardOverflow>
     </Card>
