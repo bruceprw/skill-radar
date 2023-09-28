5016aa8030e26081319b25bc4d3646014b6a2c94
lukedale90 <51388504+lukedale90@users.noreply.github.com>
2023-09-28 10:57:28 +0100

    BadgeMapper

--- src/components/skillCard.jsx
+++ src/components/skillCard.jsx
@@ -1,33 +1,34 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import * as React from "react";
+import Avatar from "@mui/joy/Avatar";
+import Chip from "@mui/joy/Chip";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import ButtonGroup from "@mui/joy/ButtonGroup";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import CardOverflow from "@mui/joy/CardOverflow";
+import CardActions from "@mui/joy/CardActions";
+import IconButton from "@mui/joy/IconButton";
+import Typography from "@mui/joy/Typography";
+import SvgIcon from "@mui/joy/SvgIcon";
+import UserBadges from "./UserBadges";
+import { getBadgeIcon } from "./BadgeMapper";
 
-export const SkillCard = (props) => {
-  
+export default function SkillCard(props) {
+  const icon = getBadgeIcon(props.id);
+
   return (
     <Card
       sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        height: 200,
-        width: 150,
+        maxWidth: "100%",
       }}
     >
-      <CardContent>
-        <Typography
-          align="center"
-          gutterBottom
-          variant="h6"
-        >
-          {props.title}
-        </Typography>
-        <Typography
-          align="center"
-          variant="body2"
-        >
-          {props.description}
-        </Typography>
+      <CardContent sx={{ alignItems: "center", textAlign: "center" }}>
+        <Avatar> {props.description}</Avatar>
+        <Avatar> {icon}</Avatar>
+        <Typography level="title-lg">{props.title}</Typography>
       </CardContent>
+      <CardOverflow sx={{ bgcolor: "background.level1" }}></CardOverflow>
     </Card>
   );
-};
-
-export default SkillCard;
+}
